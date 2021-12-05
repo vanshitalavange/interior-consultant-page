01d74177753b2f413308dafd07c5066869546e55
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-12-06 00:24:17 +0530

    feat: logic done for rendering different elements

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+var btnMenu = document.querySelector('.btn-menu');
+var mobileMenu = document.querySelector('.mobile-menu');
+var btn = document.querySelector('.btn');
+var homePage = document.querySelector('.page');
+var btnClose = document.querySelector('.btn-close');
+function openMenu(){
+   mobileMenu.style.display = "block";
+   homePage.style.display = "none";
+}
+
+function closeMenu(){
+  mobileMenu.style.display = "none";
+  homePage.style.display = "flex";
+}
+
+btnMenu.addEventListener("click",openMenu);
+
+btnClose.addEventListener("click",closeMenu);
+
+
+
 function eventHandler() {
   var deviceWidth = document.body.clientWidth;
   if (deviceWidth > 480) {
-    window.location.href = "index.html";
-  }
+     closeMenu();
+ }
 }
 
 window.addEventListener("resize", eventHandler);
+
+
+
+
+
+
+
